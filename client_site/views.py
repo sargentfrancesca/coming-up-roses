042a7530ec4bcd627e066565309b0db04629899c
Francesca Sargent <ladyquas@francescas-mbp.home>
2017-01-15 17:40:52 +0000

    Added smtp sending settings

--- client_site/views.py
+++ client_site/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 from django.core import serializers
 from django.template import Context, loader, RequestContext
 from client_site.forms import ContactForm
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import redirect
 from django.template import Context
 from django.template.loader import get_template
@@ -44,40 +44,22 @@ class PriceView(generic.TemplateView):
 def contact(request):
     form_class = ContactForm
 
-    # new logic!
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
         if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            form_content = request.POST.get('content', '')
-
-            # Email the profile with the 
-            # contact information
-            template = get_template('contact_template.txt')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-            })
-            content = template.render(context)
-
-            email = EmailMessage(
-                "New contact form submission",
-                content,
-                "Coming Up Roses Queries" +'',
-                ['contact@cominguproses.co.uk'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.send()
+            contact_name = form.cleaned_data['contact_name']
+            contact_email = form.cleaned_data['contact_email']
+            contact_phone = form.cleaned_data['contact_phone']
+            content = form.cleaned_data['content']
             
-            return redirect('contact')
+            send_mail(contact_name, content, contact_email, ['contact@cominguproses.co.uk'])
+            return redirect('success')
     
     return render(request, 'contact.html', {
         'form': form_class,
     })
+
+def success(request):
+    return HttpResponse('Success! Thank you for your message.')
